70d7200464a848d02699b72c3e122b959076f9e3
nnzhuilian <609117168@qq.com>
2018-09-20 21:49:15 +0800

    Revert "9.20-3"
    This reverts commit 8ed340f6bddf7b672d2ba564eab97872f1596f48.

--- Quesan/Quesan/src/main/java/com/hxh/Quesan/dao/UserDAO.java
+++ Quesan/Quesan/src/main/java/com/hxh/Quesan/dao/UserDAO.java
@@ -6,12 +6,12 @@ import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface UserDAO {
-    @Insert({"insert into user (name,password,salt,head_url) values (#{name},#{password},#{salt},#{headUrl})"})
+    @Insert({"insert into user (name,password,salt,headUrl) values (#{name},#{password},#{salt},#{headUrl})"})
     int addUser(User user);
-    @Select({"select name,password,salt,head_url from user where id=#{id}"})
+    @Select({})
     User selectById(int id);
-    @Update({"update user set password=#{password} where id=#{id}"})
-    void updatePassword(User user);
-    @Delete({"delete from user where id=#{id}"})
-    void deleteById(int id);
+    @Update({})
+
+    @Delete({})
+
 }
