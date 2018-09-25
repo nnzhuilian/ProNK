94d7269461bf144fa024bc50859fafeae4f2854b
nnzhuilian <609117168@qq.com>
2018-09-25 13:33:42 +0800

    9.25-a

--- Quesan/Quesan/src/main/java/com/hxh/Quesan/controller/LoginContoller.java
+++ Quesan/Quesan/src/main/java/com/hxh/Quesan/controller/LoginContoller.java
@@ -6,10 +6,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 @Controller
@@ -18,15 +22,21 @@ public class LoginContoller {
 @Autowired
     UserService userService;
 
-    @RequestMapping(path={"/login"},method = {RequestMethod.POST})
-    public String login(Model model,
+    @RequestMapping(path={"/register/"},method = {RequestMethod.POST})
+    public String register(Model model,
                         @RequestParam("username") String username,
-                        @RequestParam("password") String password ){
+                        @RequestParam("password") String password,
+                        HttpServletResponse httpServletResponse){
         try {
             Map<String, String> map = userService.register(username, password);
-            if (map.containsKey("map")) {
+            if (map.containsKey("msg")) {
                 model.addAttribute("msg", map.get("msg"));
                 return "login";
+            }
+            if(map.containsKey("ticket")){
+                Cookie cookie=new Cookie("ticket",map.get("ticket"));
+                cookie.setPath("/");
+                httpServletResponse.addCookie(cookie);
             }
             return "redirect:/";
         }catch (Exception e){
@@ -34,6 +44,30 @@ public class LoginContoller {
             return "login";
         }
     }
+    @RequestMapping(path={"/login/"},method = {RequestMethod.POST})
+    public String login(Model model,
+                        @RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        HttpServletResponse httpServletResponse){
+        try {
+            Map<String, String> map = userService.login(username, password);
+            if (map.containsKey("msg")) {
+                model.addAttribute("msg", map.get("msg"));
+                return "login";
+            }
+            if(map.containsKey("ticket")){
+                Cookie cookie=new Cookie("ticket",map.get("ticket"));
+                cookie.setPath("/");
+                httpServletResponse.addCookie(cookie);
+            }
+            return "redirect:/";
+        }catch (Exception e){
+            logger.error("登陆异常:"+e.getMessage());
+            return "login";
+        }
+    }
+
+
     @RequestMapping(path={"/reglogin"},method = {RequestMethod.GET})
     public String reg(Model model){
         return "login";
--- Quesan/Quesan/src/main/java/com/hxh/Quesan/service/UserService.java
+++ Quesan/Quesan/src/main/java/com/hxh/Quesan/service/UserService.java
@@ -1,15 +1,14 @@
 package com.hxh.Quesan.service;
 
+import com.hxh.Quesan.dao.LoginTicketDAO;
 import com.hxh.Quesan.dao.UserDAO;
+import com.hxh.Quesan.model.LoginTicket;
 import com.hxh.Quesan.model.User;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 import static com.hxh.Quesan.util.MD5Ari.MD5;
 
@@ -17,6 +16,8 @@ import static com.hxh.Quesan.util.MD5Ari.MD5;
 public class UserService {
     @Autowired
     private UserDAO userDAO;
+    @Autowired
+    LoginTicketDAO loginTicketDAO;
 
     public Map<String,String> register(String username,String password){
         Map<String,String> map=new HashMap<String, String>();
@@ -35,10 +36,46 @@ public class UserService {
         user.setSalt(UUID.randomUUID().toString().substring(0,5));
         user.setPassword(MD5(password+user.getSalt()));
         userDAO.addUser(user);
+
+        String ticket=addLoginTicket(user.getId());
+        map.put("ticket",ticket);
+
+        return map;
+    }
+    public Map<String ,String> login(String username,String password){
+        Map<String,String> map=new HashMap<String,String>();
+        if((StringUtils.isBlank(username))||(StringUtils.isBlank(password))){
+            map.put("msg","用户名或密码不能为空");
+            return map;
+        }
+        User user=userDAO.selectByName(username);
+        if (user==null){
+            map.put("msg","用户名不存在");
+            return map;
+        }
+        else{
+            if(!(user.getPassword()).equals(MD5(password+user.getSalt()))){
+                map.put("msg","用户名或密码错误");
+                return map;
+            }
+        }
+        String ticket=addLoginTicket(user.getId());
+        map.put("ticket",ticket);
         return map;
     }
     public User getUser(int id){
         return userDAO.selectById(id);
+    }
+    public String addLoginTicket(int userId){
+        LoginTicket ticket=new LoginTicket();
+        ticket.setUserId(userId);
+        Date now=new Date();
+        now.setTime(now.getTime()+3600*24*100);
+        ticket.setExpired(now);
+        ticket.setStatus(0);
+        ticket.setTicket(UUID.randomUUID().toString().replaceAll("-",""));
+        loginTicketDAO.addTicket(ticket);
+        return ticket.getTicket();
     }
 
 
